33beee6817ec93acb2d75e26ed27c8fd4e51578a
Wassim67_ <wjallabi@numerique-educatif.fr>
2022-10-21 10:46:29 +0200

    Rajout des éléments manquants (mutateur)

--- Concession.cs
+++ Concession.cs
@@ -10,12 +10,16 @@ namespace CreditCeleste  // Mon tp
     {
 
        private string monNomConcession; // attribut propre à l'objet
-        private string monAdresseConcession;
+       private string telConcession;
+       private string mailConcession;
+       private string regionConcession;
+       private string villeConcession;
+       private string stockVehicule;
 
 
         // collection de vendeurs
         private List<Vendeur> lesVendeurs = new List<Vendeur>();
-        // ilfaut créer la class vendeur
+        // il faut créer la class vendeur
 
 
         public static string maRegion = "Alsace";  // attribut de classe
@@ -24,18 +28,77 @@ namespace CreditCeleste  // Mon tp
         public Concession()
         {
         }
-        public Concession(string NomConcession, string AdresseConcession)
+        public Concession(string NomConcession, string laRegionConcession, string villeConcession)
         {
             monNomConcession = NomConcession;
-            monAdresseConcession = AdresseConcession;
+            regionConcession = laRegionConcession;
+            villeConcession = this.villeConcession;
         }
 
         // -- Mutateurs 
         // Propriétes
-        public string MonAdresseConsession { get => monAdresseConcession; set => monAdresseConcession = value; }
+        //public string MonAdresseConsession { get => monAdresseConcession; set => monAdresseConcession = value; }
+
+        public string getMonNomConcession() // ok
+        {
+            return monNomConcession;
+        }
+
+        public void setMonNomConcession(string leNomConcess) // ok
+        {
+            monNomConcession = leNomConcess;
+        }
+
+        public string getTelConcession() //ok
+        {
+            return telConcession;
+        }
+
+        public void setTelConcession(string leTelConcession) //ok
+        {
+            telConcession = leTelConcession;
+        }
+
+        public void setMailConcession(string leMailConcession) //ok
+        {
+            mailConcession = leMailConcession;
+        }
+
+        public string getMailConcession() //ok
+        {
+            return mailConcession;
+        }
+
+        public void setRegionConcession(string laRegionConcession)
+        {
+            regionConcession = laRegionConcession;
+        }
+
+        public string getRegionConcession()
+        {
+            return regionConcession;
+        }
+
+        public void setVilleConcession(string laVilleConcession)
+        {
+            villeConcession = laVilleConcession;
+        }
+
+        public string getVilleConcession()
+        {
+            return villeConcession;
+        }
+
+        public void setStockVehicule(string leStockVehicule)
+        {
+            stockVehicule = leStockVehicule;
+        }
+        public string getStockVehicule()
+        {
+            return stockVehicule;
+        }
 
         // pour rajouter un vendeur
-
         public void ajoutVendeur(Vendeur unVendeur)
         {
             lesVendeurs.Add(unVendeur);
